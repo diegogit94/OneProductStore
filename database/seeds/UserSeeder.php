a49db3e17ad8f8c6e187ee7f5db0b2818e45db09
Diego Ospina <diego.ospinagp@gmail.com>
2020-12-18 15:29:11 -0500

    db: added shopping data to users migration and modified the seeder

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -15,10 +15,20 @@ class UserSeeder extends Seeder
         DB::table('users')->insert([
             'id' => 1,
             'name' => 'Diego',
+            'surname' => 'Ospina',
+            'document_type' => 'CC',
+            'document' => '1037632900',
+            'mobile' => '3053220512',
+            'address' => 'Diagonal 29 #28 Sur 33',
+            'city' => 'Envigado',
+            'province' => 'Antioquia',
+            'postal_code' => '055420',
+            'country' => 'Colombia',
             'email' => 'diego@oneproductstore.com',
             'password' => bcrypt('123456789'),
             'created_at' => now(),
-            'updated_at' => now()
+            'updated_at' => now(),
+            'email_verified_at' => now(),
         ]);
     }
 }
