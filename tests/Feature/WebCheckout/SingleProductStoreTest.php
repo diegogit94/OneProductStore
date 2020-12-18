7d8f6bb471907e2c5d470d538880046d5a77ecb3
Diego Ospina <diego.ospinagp@gmail.com>
2020-12-18 02:11:56 -0500

    test: added test to authenticated users

--- tests/Feature/WebCheckout/SingleProductStoreTest.php
+++ tests/Feature/WebCheckout/SingleProductStoreTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature\WebCheckout;
 
 use App\Product;
+use App\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -23,5 +24,30 @@ class SingleProductStoreTest extends TestCase
             ->assertSee($product->name)
             ->assertSee($product->price)
             ->assertSee($product->image);
+    }
+
+    /** @test */
+    public function an_authenticated_user_can_access_to_form()
+    {
+        $product = factory(Product::class)->create();
+
+        $user = factory(User::class)->create();
+
+        $response = $this->actingAs($user)->post(route('form.index', $product));
+        
+        $response->assertOk()
+            ->assertViewIs('form')
+            ->assertViewHas(['product']);
+    }
+
+    /** @test */
+    public function a_non_authenticated_user_cannot_access_to_form()
+    {
+        $product = factory(Product::class)->create();
+
+        $response = $this->post(route('form.index', $product));
+
+        $response->assertRedirect('/login')
+            ->assertDontSee('product');
     }
 }
