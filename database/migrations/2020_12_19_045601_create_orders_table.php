48746c935a60b4b42227e23a082d04e1a5b8fddc
Diego Ospina <diego.ospinagp@gmail.com>
2020-12-19 14:39:40 -0500

    db: created orders table

---
+++ database/migrations/2020_12_19_045601_create_orders_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->string('request_id')->nullable();
+            $table->string('reference')->nullable();
+            $table->string('description')->nullable();
+            $table->double('total')->nullable();
+            $table->string('status')->nullable()->default('PENDING');
+
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
